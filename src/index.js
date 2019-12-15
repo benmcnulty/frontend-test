26fff220f612a6df1f9a7ee7641984f976ccd267
Ben McNulty <benmcnulty@live.com>
2019-12-15 03:21:05 -0500

    More responsive adjustments and layout work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as PIXI from 'pixi.js';
 /* Aliases */
+window.PIXI = PIXI;
 let Application = PIXI.Application,
     Container = PIXI.Container,
     Sprite = PIXI.Sprite;
@@ -29,7 +30,7 @@ let width = window.innerWidth || document.documentElement.clientWidth || documen
 let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 let totalWidth = width;
 let totalHeight = height;
-let showdownHeight = Math.round(totalHeight*0.25);
+let showdownHeight = Math.round(totalHeight*0.4);
 let spinnerHeight = (totalHeight-showdownHeight);
 
 function sizeCheck() {
@@ -42,7 +43,7 @@ function sizeCheck() {
 }
 
 function canvasResizer(newWidth, newHeight) {    
-    showdownHeight = Math.round(newHeight*0.25);
+    showdownHeight = Math.round(newHeight*0.4);
     spinnerHeight = (newHeight-showdownHeight);
     showdownCanvas.stage.position.x = 0;
     showdownCanvas.stage.position.y = 0;
@@ -55,7 +56,6 @@ function canvasResizer(newWidth, newHeight) {
     spinnerCanvas.stage.width = newWidth;
     spinnerCanvas.stage.height = spinnerHeight;
     spinnerCanvas.renderer.resize(newWidth, spinnerHeight);
-    console.log("resized: " + showdownHeight);
 }
 
 window.addEventListener('load', sizeCheck);
@@ -64,9 +64,17 @@ window.addEventListener('orientationchange', sizeCheck);
 
 /* Get JSON Data */
 let jsonResponse = {};
+let spinRequested = false;
+let spinTime = false;
+let result = false;
 
 function showData() {
-    console.warn(jsonResponse.POSITION);
+    if (!result) {
+        let spinfo = jsonResponse.POSITION;
+        console.warn(spinfo);
+        setTimeout(function(){ spinTime = false; }, 5000);
+        console.warn(spinnerCanvas.loader.resources.spinnerButton.texture);
+    }
 };
 
 function setData() {
@@ -76,15 +84,17 @@ function setData() {
     req.open('GET', url, true);
     req.onload = function() {
         jsonResponse = JSON.parse(req.responseText);
-        console.warn(jsonResponse);
         showData();
+        spinRequested = false;
+        spinTime = true;
+        console.warn("Spin done? " + !spinRequested);
     };
     req.send(null);
-    console.warn(message);
+    console.warn("Spin done? " + !spinRequested);
 };
 
 /* Handle Input */
-let touchType = "click";
+let touchType;
 let message = "no message";
 
 if ('ontouchstart' in window) {
@@ -94,8 +104,6 @@ if ('ontouchstart' in window) {
     touchType = "click";
     message = "clicked";
 }
-
-document.getElementById("spinnerAnimation").addEventListener(touchType, setData);
 
 /* Pixi Loaders */
 showdownCanvas.loader.add([
@@ -111,18 +119,15 @@ spinnerCanvas.loader.add([
 
 function loadProgressHandler( loader, resource ) {
     console.log("file: " + resource.name);
-    console.log("loading: " + resource.url);
-    console.log("progress: " + loader.progress + "%");
-    console.log("loading: " + resource.name);
 }
 
 function showdownSetup() {
-    showdownHeight = Math.round(totalHeight*0.25);
+    showdownHeight = Math.round(totalHeight*0.4);
     let showdownContainerWidth = totalWidth;
     let showdownCenter = showdownContainerWidth/2;
     let showdownMiddle = showdownHeight/2;
 
-    showdownCanvas.stage.position.x = 0;
+    showdownCanvas.stage.position.x = (showdownCenter/2);
     showdownCanvas.stage.position.y = 0;
     showdownCanvas.stage.width = showdownContainerWidth;
     showdownCanvas.stage.height = showdownHeight;
@@ -159,41 +164,56 @@ function spinnerSetup() {
     spinnerHeight = (height-showdownHeight);
 
     let spinnerContainerWidth = totalWidth;
-    let spinnerCenter = spinnerContainerWidth/2;
-    let spinnerMiddle = spinnerHeight/2;
-
-    spinnerCanvas.stage.position.x = 0;
-    spinnerCanvas.stage.position.y = 0;
-    spinnerCanvas.stage.width = spinnerContainerWidth;
-    spinnerCanvas.stage.height = spinnerHeight;
+    let spinnerCenter = (spinnerContainerWidth/2);
+    let spinnerMiddle = (spinnerHeight/2);
 
     const spinnerContainer = new Container();
     spinnerContainer.position.set(spinnerCenter,spinnerMiddle);
+    spinnerContainer.scale.set(1,1);
     
     const wheelSprite = 
     new Sprite(spinnerCanvas.loader.resources.spinnerWheel.texture);
     wheelSprite.anchor.set(0.5);
-    wheelSprite.width = 0.9*(spinnerContainerWidth);
+    wheelSprite.width = (wheelSprite.width)*0.6;
     wheelSprite.height = wheelSprite.width;
     wheelSprite.pivot.x = 0;
     wheelSprite.pivot.y = 0;
     wheelSprite.position.x = 0;
-    wheelSprite.position.y = 0;
+    wheelSprite.position.y = (wheelSprite.width)*0.2;
+    spinnerCanvas.ticker.add((delta) => {
+        if (spinRequested || spinTime) {
+            wheelSprite.rotation += 0.02 * delta;
+        } else {
+            return;
+        }
+    });
 
     const buttonSprite = 
     new Sprite(spinnerCanvas.loader.resources.spinnerButton.texture);
+    let halfWidth = (buttonSprite.width/2);
     buttonSprite.anchor.set(0.5);
-    buttonSprite.width = 0.9*(spinnerContainerWidth);
+    buttonSprite.width = buttonSprite.width;
     buttonSprite.height = (buttonSprite.width*0.2);
-    buttonSprite.position.x = 0;
-    buttonSprite.position.y = (wheelSprite.height)/-1.75;    
+    buttonSprite.interactive = true;
+    buttonSprite.buttonMode = true;
+    buttonSprite.position.x = 10;
+    buttonSprite.position.y = -150;
+    buttonSprite.on(touchType, (event) => {
+        if (!spinRequested) {
+            spinRequested = true;
+            setData();
+        } else {
+            return;
+        }
+    });
 
     const markerSprite = 
     new Sprite(spinnerCanvas.loader.resources.spinnerMarker.texture);
     markerSprite.anchor.set(0.5);
     markerSprite.pivot.x = 0;
     markerSprite.pivot.y = 0;
-    markerSprite.position.set((spinnerContainerWidth*0.3),-(spinnerContainerWidth*0.3));
+    markerSprite.position.x = 100;
+    markerSprite.position.y = -90;
     markerSprite.rotation = 0.5;
     
     spinnerContainer.addChild(wheelSprite);
